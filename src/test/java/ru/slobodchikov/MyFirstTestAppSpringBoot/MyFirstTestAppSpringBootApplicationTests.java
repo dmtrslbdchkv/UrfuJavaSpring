9081d80585a985f2db8b517f0b504f61a02bd9d1
Dmitry Slobodchikov <dmitryslo@yandex.ru>
2024-09-18 19:43:41 +0500

    First commit

---
+++ src/test/java/ru/slobodchikov/MyFirstTestAppSpringBoot/MyFirstTestAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.slobodchikov.MyFirstTestAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstTestAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
